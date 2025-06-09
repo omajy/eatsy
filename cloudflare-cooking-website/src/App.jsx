784a4726749ab933852c6ec888173e763c66f226
user.email <126528582+UoAomaj@users.noreply.github.com>
2025-06-09 19:21:36 +0930

    eatsy logo addition

--- cloudflare-cooking-website/src/App.jsx
+++ cloudflare-cooking-website/src/App.jsx
@@ -6,7 +6,6 @@ import RecipeCard from './components/RecipeCard'
 import recipes from './data/mockRecipes';
 
 function App() {
-  
   const [currentRecipe, setCurrentRecipe] = useState(0);
   
   const nextRecipe = () => {
@@ -20,31 +19,34 @@ function App() {
   const recipe = recipes[currentRecipe];
   
   return (
-    <div className="h-screen flex items-center justify-center relative">
-      <button
-  onClick={prevRecipe}
-  className="absolute left-8 text-7xl text-grey-800 font-extrabold cursor-pointer select-none"
-  style={{ all: 'unset', cursor: 'pointer', position: 'absolute', left: 'calc(50% - 220px)' }}
->
-  ‹
-</button>
-      <div>
-        <RecipeCard 
-        title={recipe.title}
-        image={recipe.image}
-        cookTime={recipe.cookTime}
-        servings={recipe.servings}
-        ingredients={recipe.ingredients}
-        instructions={recipe.instructions}
-        />
+    <div className="relative w-full">
+      <h1 className="fixed top-12 left-20 text-6xl font-['Monsieur_La_Doulaise'] text-[#5C4033] tracking-wide">eatsy</h1>
+      <div className="h-screen flex items-center justify-center">
+        <button
+          onClick={prevRecipe}
+          className="absolute text-7xl text-grey-800 font-extrabold cursor-pointer select-none"
+          style={{ all: 'unset', cursor: 'pointer', position: 'absolute', left: 'calc(50% - 220px)' }}
+        >
+          ‹
+        </button>
+        <div>
+          <RecipeCard 
+            title={recipe.title}
+            image={recipe.image}
+            cookTime={recipe.cookTime}
+            servings={recipe.servings}
+            ingredients={recipe.ingredients}
+            instructions={recipe.instructions}
+          />
+        </div>
+        <button
+          onClick={nextRecipe}
+          className="absolute text-7xl text-red font-extrabold cursor-pointer select-none"
+          style={{ all: 'unset', cursor: 'pointer', position: 'absolute', right: 'calc(50% - 220px)' }}
+        >
+          ›
+        </button>
       </div>
-      <button
-  onClick={nextRecipe}
-  className="absolute right-8 text-7xl text-red font-extrabold cursor-pointer select-none"
-  style={{ all: 'unset', cursor: 'pointer', position: 'absolute', right: 'calc(50% - 220px)' }}
->
-  ›
-</button>
     </div>
   );
 }
