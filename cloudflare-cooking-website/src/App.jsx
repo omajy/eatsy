673a6e3c5a2bda540ef283c7f9ca5a6061c732ba
user.email <126528582+UoAomaj@users.noreply.github.com>
2025-06-10 17:28:37 +0930

    filter button non implemented

--- cloudflare-cooking-website/src/App.jsx
+++ cloudflare-cooking-website/src/App.jsx
@@ -17,9 +17,22 @@ function App() {
   }
   
   const recipe = recipes[currentRecipe];
-  
+
+  const [showFilters, setShowFilters] = useState(false);
+
   return (
     <div className="relative w-full">
+      <div className="fixed top-12 right-20 z-30 tracking-wide text-[#5C4033]">
+        <div className="relative inline-block w-48">
+        <button onClick={() => setShowFilters(!showFilters)}  
+        className="bg-transparent px-4 py-2 rounded shadow cursor-pointer hover:bg-gray-100 transition">Filters</button>
+        {showFilters && (
+        <div className="absolute mt-2 bg-white border rounded shadow p-4 w-48">
+          <p>Dropdown content goes here</p>
+        </div>
+         )}
+        </div>
+      </div>
       <h1 className="fixed top-12 left-20 text-6xl font-['Monsieur_La_Doulaise'] text-[#5C4033] tracking-wide">eatsy</h1>
       <div className="h-screen flex items-center justify-center">
         <button
